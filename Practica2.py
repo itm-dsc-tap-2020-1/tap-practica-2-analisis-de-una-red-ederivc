e8e2f4adef22d671d4bac9ffc0688939c53b43de
ederivc <61289461+ederivc@users.noreply.github.com>
2020-02-27 11:56:28 -0600

    Add files via upload

---
+++ Practica2.py
@@ -0,0 +1,52 @@
+import os
+
+red = "200.33.171.0"
+
+os.system("nmap -sP " + red+ "/24")
+
+# Starting Nmap 7.60 ( https://nmap.org ) at 2020-02-27 11:54 CST
+# Nmap scan report for mail.itmorelia.edu.mx (200.33.171.3)
+# Host is up (0.18s latency).
+# Nmap scan report for delfin2.itmorelia.edu.mx (200.33.171.11)
+# Host is up (0.090s latency).
+# Nmap scan report for 200.33.171.13
+# Host is up (0.13s latency).
+# Nmap scan report for dsc.itmorelia.edu.mx (200.33.171.20)
+# Host is up (0.10s latency).
+# Nmap scan report for libra.itmorelia.edu.mx (200.33.171.54)
+# Host is up (0.26s latency).
+# Nmap scan report for 200.33.171.63
+# Host is up (0.11s latency).
+# Nmap scan report for sagitario.itmorelia.edu.mx (200.33.171.65)
+# Host is up (0.11s latency).
+# Nmap scan report for 200.33.171.66
+# Host is up (0.099s latency).
+# Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+# Host is up (0.11s latency).
+# Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+# Host is up (0.096s latency).
+# Nmap scan report for 200.33.171.85
+# Host is up (0.089s latency).
+# Nmap scan report for 200.33.171.97
+# Host is up (0.100s latency).
+# Nmap scan report for 200.33.171.98
+# Host is up (0.091s latency).
+# Nmap scan report for 200.33.171.99
+# Host is up (0.091s latency).
+# Nmap scan report for 200.33.171.115
+# Host is up (0.087s latency).
+# Nmap scan report for 200.33.171.118
+# Host is up (0.092s latency).
+# Nmap scan report for vinculacion.itmorelia.edu.mx (200.33.171.124)
+# Host is up (0.11s latency).
+# Nmap scan report for 200.33.171.125
+# Host is up (0.078s latency).
+# Nmap scan report for 200.33.171.127
+# Host is up (0.078s latency).
+# Nmap scan report for 200.33.171.128
+# Host is up (0.091s latency).
+# Nmap scan report for 200.33.171.160
+# Host is up (0.10s latency).
+# Nmap scan report for 200.33.171.254
+# Host is up (0.097s latency).
+# Nmap done: 256 IP addresses (22 hosts up) scanned in 4.75 seconds
